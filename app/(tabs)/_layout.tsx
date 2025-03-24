9233b11de94108a2279b7b7f7591895330e7dc90
mutiararflnsh <rifliansahmutiara@gmail.com>
2025-03-24 08:33:56 +0700

    feat: ionicons tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,74 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function Layout() {
   return (
     <Tabs>
-      <Tabs.Screen name="Home" />
-      <Tabs.Screen name="LoginPage" />
-      <Tabs.Screen name="RegisterPage" />
-      <Tabs.Screen name="Transfer" />
-      <Tabs.Screen name="TopUpPage" />
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: "My Wallet",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="wallet" color={color} size={size} />
+          ),
+          headerStyle: {
+            backgroundColor: "#0061FF",
+          },
+          headerTintColor: "white",
+        }}
+      />
+      <Tabs.Screen
+        name="LoginPage"
+        options={{
+          title: "Login",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+          headerStyle: {
+            backgroundColor: "#0061FF",
+          },
+          headerTintColor: "white",
+        }}
+      />
+      <Tabs.Screen
+        name="RegisterPage"
+        options={{
+          title: "Register",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-add" color={color} size={size} />
+          ),
+          headerStyle: {
+            backgroundColor: "#0061FF",
+          },
+          headerTintColor: "white",
+        }}
+      />
+      <Tabs.Screen
+        name="Transfer"
+        options={{
+          title: "Transfer",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="paper-plane" color={color} size={size} />
+          ),
+          headerStyle: {
+            backgroundColor: "#0061FF",
+          },
+          headerTintColor: "white",
+        }}
+      />
+      <Tabs.Screen
+        name="TopUpPage"
+        options={{
+          title: "Top Up",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="card" color={color} size={size} />
+          ),
+          headerStyle: {
+            backgroundColor: "#0061FF",
+          },
+          headerTintColor: "white",
+        }}
+      />
       {/* <Tabs.Screen name="explore" /> */}
     </Tabs>
   );
